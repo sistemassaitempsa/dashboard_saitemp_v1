524a33bef80ee4de0d3ee81473bdef56567af844
sistemassaitempsa <sistemassaitempsa@saitempsa.com>
2024-12-19 17:14:35 -0500

    se hace cambio para mostrar la fecha de actualizacion

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -1911,6 +1911,8 @@ export default {
         year: "numeric",
         month: "short",
         day: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
       };
       return valor.toLocaleDateString("es-ES", opciones);
     },
